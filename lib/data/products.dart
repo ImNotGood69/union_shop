46ec128d59e7c690a3415d87686ed5c731cc99d4
Dominykas Sicas <Domsu60@gmail.com>
2025-12-01 14:33:03 +0000

    removed unnecessar comments left by friend who helped

--- lib/data/products.dart
+++ lib/data/products.dart
@@ -6,27 +6,27 @@ final List<Product> products = [
     title: 'Sage Hoodie',
     price: '£45.00',
     imageUrl:
-        'https://shop.upsu.net/cdn/shop/files/SageHoodie_530x530@2x.png?v=1745583498', // ← Replace with your image URL
+        'https://shop.upsu.net/cdn/shop/files/SageHoodie_530x530@2x.png?v=1745583498',
   ),
   Product(
     id: 'p2',
     title: 'Signature T-Shirt Indigo Blue',
     price: '£20.00',
     imageUrl:
-        'https://shop.upsu.net/cdn/shop/files/Signature_T-Shirt_Indigo_Blue_2_530x530@2x.jpg?v=1758290534', // ← Replace with your image URL
+        'https://shop.upsu.net/cdn/shop/files/Signature_T-Shirt_Indigo_Blue_2_530x530@2x.jpg?v=1758290534',
   ),
   Product(
     id: 'p3',
     title: 'Placeholder Product 3',
     price: '£20.00',
     imageUrl:
-        'https://shop.upsu.net/cdn/shop/files/GreyHoodieFinal_1024x1024@2x.jpg?v=1742201957', // ← Replace with your image URL
+        'https://shop.upsu.net/cdn/shop/files/GreyHoodieFinal_1024x1024@2x.jpg?v=1742201957', 
   ),
   Product(
     id: 'p4',
     title: 'Placeholder Product 4',
     price: '£25.00',
     imageUrl:
-        'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQxjQc4yYW3O8ar5THil6vIY_hw2Xat5xiWig&s', // ← Replace with your image URL
+        'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQxjQc4yYW3O8ar5THil6vIY_hw2Xat5xiWig&s', 
   ),
 ];
