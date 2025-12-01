95cf0c57633e816177c7b7dab7e9e95613426f09
Dominykas Sicas <Domsu60@gmail.com>
2025-12-01 14:30:29 +0000

    added images to product page

--- lib/data/products.dart
+++ lib/data/products.dart
@@ -3,30 +3,30 @@ import 'package:union_shop/models/product.dart';
 final List<Product> products = [
   Product(
     id: 'p1',
-    title: 'Placeholder Product 1',
-    price: '£10.00',
+    title: 'Sage Hoodie',
+    price: '£45.00',
     imageUrl:
-        'https://shop.upsu.net/cdn/shop/files/PortsmouthCityMagnet1_1024x1024@2x.jpg?v=1752230282',
+        'https://shop.upsu.net/cdn/shop/files/SageHoodie_530x530@2x.png?v=1745583498', // ← Replace with your image URL
   ),
   Product(
     id: 'p2',
-    title: 'Placeholder Product 2',
-    price: '£15.00',
+    title: 'Signature T-Shirt Indigo Blue',
+    price: '£20.00',
     imageUrl:
-        'https://shop.upsu.net/cdn/shop/files/PortsmouthCityMagnet1_1024x1024@2x.jpg?v=1752230282',
+        'https://shop.upsu.net/cdn/shop/files/Signature_T-Shirt_Indigo_Blue_2_530x530@2x.jpg?v=1758290534', // ← Replace with your image URL
   ),
   Product(
     id: 'p3',
     title: 'Placeholder Product 3',
     price: '£20.00',
     imageUrl:
-        'https://shop.upsu.net/cdn/shop/files/PortsmouthCityMagnet1_1024x1024@2x.jpg?v=1752230282',
+        'https://shop.upsu.net/cdn/shop/files/GreyHoodieFinal_1024x1024@2x.jpg?v=1742201957', // ← Replace with your image URL
   ),
   Product(
     id: 'p4',
     title: 'Placeholder Product 4',
     price: '£25.00',
     imageUrl:
-        'https://shop.upsu.net/cdn/shop/files/PortsmouthCityMagnet1_1024x1024@2x.jpg?v=1752230282',
+        'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQxjQc4yYW3O8ar5THil6vIY_hw2Xat5xiWig&s', // ← Replace with your image URL
   ),
 ];
